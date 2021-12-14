2e2112f7667857b18ec05b3707576e5b754c66fb
Henrique Marques <hfvmarques@gmail.com>
2021-12-13 22:26:55 -0400

    only odds

---
+++ SomenteImpares/SomenteImpares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SomenteImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+
+            var odd = numeros.Where(x => x % 2 == 0);
+            var qnt = odd.Count();
+
+            if (qnt == 0)
+            {
+                Console.WriteLine("Este array possui somente números ímpares.");
+            }
+            else
+            {
+                Console.WriteLine("Este array possui os seguintes números pares:");
+                foreach (var p in odd)
+                {
+                    Console.Write(p+" ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
