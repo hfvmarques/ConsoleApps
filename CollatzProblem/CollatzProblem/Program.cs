4d15332bfc7aa0aada0270127a86f75c9a4191d5
Henrique Marques <hfvmarques@gmail.com>
2021-12-13 22:26:38 -0400

    collatz problem

---
+++ CollatzProblem/CollatzProblem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CollatzProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long larger = 0, bigger = 0;
+
+            for (int i = 1; i<=1000000;i++)
+            {
+                long counter = 1, aux = i;
+
+                while (aux != 1)
+                {
+                    var even = aux % 2;
+
+                    if (even == 0)
+                    {
+                        aux /= 2;
+                        counter++;
+                    }
+                    else
+                    {
+                        aux = (aux * 3) + 1;
+                        counter++;
+                    }
+
+                } 
+
+                if (counter > larger)
+                {
+                    larger = counter;
+                    bigger = i;
+                }
+            }
+
+            Console.WriteLine("O maior array contém " + larger + " elementos.");
+            Console.WriteLine("O número que gera o maior array é o " + bigger + ".");
+            Console.ReadLine();
+        }
+    }
+}
