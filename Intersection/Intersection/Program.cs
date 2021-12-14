743e6494f77e3c840407aadbe581cb37d292d6c5
Henrique Marques <hfvmarques@gmail.com>
2021-12-13 22:27:15 -0400

    intersection but the opposite

---
+++ Intersection/Intersection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primeiroArray = { 1, 3, 7, 29, 42, 98, 234, 93 };
+            int[] segundoArray = { 4, 6, 93, 7, 55, 32, 3 };
+
+            var intersection = primeiroArray.Except(segundoArray);
+
+            Console.WriteLine("Os números do primeiro array que não estão contidos no segundo são:");
+
+            foreach (var i in intersection)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
